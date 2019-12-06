1f49e990079dae4091b80d6172e801bead1ff947
Billy Tukija Richard <billy.tukija@onlineapp.com.br>
2019-12-06 19:59:18 -0200

    Adicionando a injecção do controller

---
+++ Sales/src/Sales.Infrastructure.Ioc/Bootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Sales.Infrastructure.Ioc
+{
+    public static class Bootstrapper
+    {
+        public static void AddDependencies(this IServiceCollection services)
+        {
+            services.AddMediatR(typeof(QuotationQueryHandler).Assembly);
+        }
+    }
+}
